ee2676d16b9ee4d3de510a1571ee6d177ddcc3d2
Vinicius Santos <vinicius.santos@akross.com.br>
2024-12-08 14:36:52 -0300

    env

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,21 +1,19 @@
-// import { config } from 'dotenv';
-import 'dotenv/config';
-//import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 import { z } from 'zod';
 
-/*if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
   dotenv.config({ path: '.env.test' });
 } else {
   dotenv.config();
-}*/
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['production', 'test', 'development']).default('production'),
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.string(),
-  //DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
-  //SECRET_KEY: z.string(),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  SECRET_KEY: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
